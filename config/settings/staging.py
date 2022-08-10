6caa44762c968d66aee7b9e305afc643ca92b193
Prashant Nigam <prashantnigam490@gmail.com>
2022-08-10 17:01:19 +0530

    Update staging.py

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -21,15 +21,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': '5432',
     }
 }
-
-
-DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
-
-print("DATABASES['default']")
 
 STATIC_HOST = ""
 
